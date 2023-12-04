988658eb37774c0257873ebf0b7017f0af305853
laissantostz <laissantostz@gmail.com>
2023-12-04 08:28:45 -0300

    cria funções adicionaNaLista e removeDaLista

--- lista.js
+++ lista.js
@@ -9,3 +9,13 @@ export function getLista() {
 export function limpaLista() {
   lista.splice(0);
 }
+
+export function adicionaNaLista(item) {
+  lista.push(item);
+}
+
+export function removeDaLista(index) {
+  if (index >= 0 && index < lista.length) {
+    lista.splice(index, 1);
+  }
+}

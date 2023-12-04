7fccc0eda06e0aab4ba405aa6d7535bab6133436
laissantostz <laissantostz@gmail.com>
2023-12-04 08:29:47 -0300

    cria limparItensDeLista() e adicionaItemNaLista()

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 // main.js
 
-import { getLista, limpaLista } from "./lista.js";
+import { getLista, limpaLista, adicionaNaLista, removeDaLista } from "./lista.js";
 
 const olItens = document.querySelector('#itens');
 const pEntrada = document.querySelector('#entrada');
 const btnAdicionar = document.querySelector('#adicionar');
 const btnLimpar = document.querySelector('#limpar');
 
+btnAdicionar.addEventListener('click', adicionaItemNaLista);
 btnLimpar.addEventListener('click', limparItensDeLista);
+
+function adicionaItemNaLista() {
+  const item = pEntrada.textContent.trim();
+  if (item) {
+    adicionaNaLista(item);
+    pEntrada.textContent = '';
+    atualizarLista(); 
+  }
+}
 
 function limparItensDeLista() {
   limpaLista();
   atualizarLista();
 }
-
-atualizarLista();
 
 function atualizarLista() {
   olItens.innerHTML = "";
@@ -22,6 +30,19 @@ function atualizarLista() {
   for (let i = 0; i < lista.length; i++) {
     const li = document.createElement('li');
     li.textContent = lista[i];
+
+    const btnRemover = document.createElement('button');
+    btnRemover.textContent = 'Remover';
+    btnRemover.onclick = function() { removerItemDaLista(i); } ;
+
+    li.appendChild(btnRemover);
     olItens.appendChild(li);
   }
 }
+function removerItemDaLista(index) {
+  removeDaLista(index);
+  atualizarLista();
+}
+
+
+atualizarLista();
